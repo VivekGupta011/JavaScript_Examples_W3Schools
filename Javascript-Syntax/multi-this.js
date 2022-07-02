4b5a4b20c6b3e2cd06697fd65d7a87d484922317
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-07-02 10:46:25 +0530

    basic of array and object done

---
+++ Javascript-Syntax/multi-this.js
@@ -0,0 +1,27 @@
+let obj={
+    firstname:"vivek",
+    lastname:'gupta',
+    id:45867,
+    fullname:function(){
+        return this.firstname + this.lastname;
+        console.log(this);~
+    },
+    address:{
+        street:"123,dhanani nagar",
+        city:"boisar",
+        getAddress:function(){
+            console.log(this);
+            console.log(this.street);
+            console.log(this.firstname);
+        }
+    }
+
+}
+console.log(obj.firstname);
+console.log(obj.lastname);
+console.log(obj.fullname());
+console.log(obj.this);
+console.log("this is 'this is keyword' " +this);
+
+// calling object inside object
+obj.address.getAddress();
---
+++ Javascript-Syntax/object.js
@@ -0,0 +1,16 @@
+
+// this is multiline object that mean if we writ ethis one line that mean this is single line object.
+let person={
+    firstname:'vivek',
+    lastname:'gupta',
+    age:21
+}
+console.log(`${person.firstname} is ${person.age} years old.`);
+
+
+
+console.log(typeof 8);
+console.log(typeof "4345");
+console.log(typeof -3);
+// Template string are powerful feature of modern javascript released in ES6.
+// it lets us insert/interpolate variable and expression into sstrings without needing to concatenate like aolder versiojns . 
